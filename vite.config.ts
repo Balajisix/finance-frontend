c87b0418f484cfd83d24bf09570049a697a6ede9
Balaji V <102216943+Balajisix@users.noreply.github.com>
2026-04-05 15:55:16 +0530

    updated url

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:4000",
+        target: "https://finance-backend-api.vercel.app",
         changeOrigin: true,
       },
     },
